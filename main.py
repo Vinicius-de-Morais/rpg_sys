2be2658afa760af8780c424fe25359e6348795e2
Vinicius-de-Morais <vmorais194@gmail.com>
2021-07-06 17:35:15 -0300

    começando a criar a classe personagem

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-class SuperClasse:
-    pass
+class Personagem:
+    def __init__(self, nome, idade, altura, peso, genero):
+        self.nome = nome.title()
+        self.raca = ""
+        self.idade = idade
+        self.altura = altura
+        self.peso = peso
+        self.genero = genero
+        self.classe = ""
+        self.vida = ""
+
+    def set_classe(self, classe):
+        
+        return self.classe
+    
+    def __str__(self):
+        print(f''' 
+        Nome: {self.nome}
+        Raça: {self.raca}
+        Idade: {self.idade}
+        Altura: {self.altura}
+        Peso: {self.peso}
+        Genero: {self.genero}
+        Classe: {self.classe}
+        ''')
+
+persona = Personagem('Jorge', 41, 1.90, '71 kg', 'Masculino')
+
+print(persona)
