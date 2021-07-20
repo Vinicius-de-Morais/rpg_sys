3dea47647442b910a1080d137f32ec5074481e65
Vinicius-de-Morais <vmorais194@gmail.com>
2021-07-19 21:10:17 -0300

    mudanças no sitema de classes

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
 class Personagem():
-    def __init__(self, nome, idade, altura, peso, genero, classe):
+    def __init__(self, nome, idade, altura, peso, genero,):
         self.nome = nome.title()
         self.raca = "Desconhecida"
         self.idade = idade
         self.altura = altura
         self.peso = peso
         self.genero = genero
-        self.classe = classe.title().strip()
         self.atributos_base = {
             'força': 0,
             'destreza': 0,
@@ -20,39 +19,20 @@ class Personagem():
         }
 
 
-    def set_classe(self):
- # Pretendo mudar e transformar cada classe em um objeto separado
- # Mas deixa isso pra outra hora.
-        if self.classe == 'Cavaleiro Andante':
-            self.atributos_base['força'] = 3
-            self.atributos_base['destreza'] = 2
-            self.atributos_base['resistencia'] = 2
-            self.atributos_base['carisma'] = 2
-        elif self.classe == 'Cavaleiro':
-            self.atributos_base['força'] = 2
-            self.atributos_base['destreza'] = 1
-            self.atributos_base['inteligencia'] = 1
-            self.atributos_base['resistencia'] = 3
-            self.atributos_base['carisma'] = 3
-        elif self.classe == 'Alquimista':
-            self.atributos_base['destreza'] = 1
-            self.atributos_base['inteligencia'] = 3
-            self.atributos_base['sabedoria'] = 2
-            self.atributos_base['resistencia'] = 1
-            self.atributos_base['carisma'] = 3
-        elif self.classe == 'Bardo':
-            self.atributos_base['inteligencia'] = 1
-            self.atributos_base['sabedoria'] = 1
-            self.atributos_base['carisma'] = 5
-            self.atributos_base['sorte'] = 3
-        elif self.classe == 'Nobre':
-            self.atributos_base = {x: 1 for x in self.atributos_base}
-        elif self.classe == 'Oraculo':
-            self.atributos_base['inteligencia'] = 1
-            self.atributos_base['fe'] = 5
-            self.atributos_base['sabedoria'] = 3
-            self.atributos_base['resistencia'] = 1    
-        return self.atributos_base
+#     def set_classe(self):
+#  # Pretendo mudar e transformar cada classe em um objeto separado
+#  # Mas deixa isso pra outra hora
+#         if self.classe == 'Cavaleiro':
+
+#         elif self.classe == 'Alquimista':
+
+#         elif self.classe == 'Bardo':
+
+#         elif self.classe == 'Nobre':
+#             self.atributos_base = {x: 1 for x in self.atributos_base}
+#         elif self.classe == 'Oraculo':
+#
+#         return self.atributos_base
     
     @property
     def atributos(self):
@@ -64,15 +44,53 @@ class Personagem():
         pass
 
     def __str__(self):
-        atributos = self.atributos_base.items()
-        for item, coiso in atributos:
-            print(item, coiso)
         return f''' 
-        Nome: {self.nome}
-        Raça: {self.raca}
-        Idade: {self.idade}
-        Altura: {self.altura}
-        Peso: {self.peso}
-        Genero: {self.genero}
-        Classe: {self.classe}
-        '''
+Nome: {self.nome}
+Raça: {self.raca}
+Idade: {self.idade}
+Altura: {self.altura}
+Peso: {self.peso}
+Genero: {self.genero}'''
+
+
+class CavaleiroAndante(Personagem):
+    def __init__(self, nome, idade, altura, peso, genero,):
+        super().__init__(nome, idade, altura, peso, genero, )
+        self.atributos_base['força'] = 3
+        self.atributos_base['destreza'] = 2
+        self.atributos_base['resistencia'] = 2
+        self.atributos_base['carisma'] = 2
+class Cavaleiro(Personagem):
+    def __init__(self, nome, idade, altura, peso, genero):
+        super().__init__(nome, idade, altura, peso, genero)
+        self.atributos_base['força'] = 2
+        self.atributos_base['destreza'] = 1
+        self.atributos_base['inteligencia'] = 1
+        self.atributos_base['resistencia'] = 3
+        self.atributos_base['carisma'] = 3
+class Alquimista(Personagem):
+    def __init__(self, nome, idade, altura, peso, genero):
+        super().__init__(nome, idade, altura, peso, genero)
+        self.atributos_base['destreza'] = 1
+        self.atributos_base['inteligencia'] = 3
+        self.atributos_base['sabedoria'] = 2
+        self.atributos_base['resistencia'] = 1
+        self.atributos_base['carisma'] = 3
+class Bardo(Personagem):
+    def __init__(self, nome, idade, altura, peso, genero):
+        super().__init__(nome, idade, altura, peso, genero)
+        self.atributos_base['inteligencia'] = 1
+        self.atributos_base['sabedoria'] = 1
+        self.atributos_base['carisma'] = 5
+        self.atributos_base['sorte'] = 3
+class Nobre(Personagem):
+    def __init__(self, nome, idade, altura, peso, genero):
+        super().__init__(nome, idade, altura, peso, genero)
+        self.atributos_base = {x: 1 for x in self.atributos_base}
+class Oraculo(Personagem):
+    def __init__(self, nome, idade, altura, peso, genero):
+        super().__init__(nome, idade, altura, peso, genero)
+        self.atributos_base['inteligencia'] = 1
+        self.atributos_base['fe'] = 5
+        self.atributos_base['sabedoria'] = 3
+        self.atributos_base['resistencia'] = 1    
--- testando.py
+++ testando.py
@@ -1,5 +1,4 @@
-from main import Personagem
+from main import Oraculo, Nobre, Bardo, Alquimista, Cavaleiro, CavaleiroAndante, Personagem
 
-persona = Personagem('Jorge', 41, 1.90, '71 kg', 'Masculino', 'oraculo')
-persona.set_classe()
-print(persona)
+persona = Nobre('Jorge', 41, 1.90, '71 kg', 'Masculino')
+print(persona.atributos)
